ef8b145993866984e0695ce210042310a6ca96ec
Akshat Gupta <akshatgtc@gmail.com>
2023-12-12 16:53:31 +0530

    deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    BACKEND_URL: "http://localhost:5000",
-    GOOGLE : "http://localhost:5000/auth/google",
+    BACKEND_URL: "https://aqify-backend.onrender.com",
+    GOOGLE : "https://aqify-backend.onrender.com/auth/google",
     FRONTEND_URL: "http://localhost:3000",
 }
