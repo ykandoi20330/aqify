76402267dce20d25c0038adb1f677d00c687af10
Akshat Gupta <akshatgtc@gmail.com>
2023-12-13 16:59:34 +0530

    Buyer fixed

--- src/config.js
+++ src/config.js
@@ -3,3 +3,9 @@ export default {
     GOOGLE : "https://aqify-backend.onrender.com/auth/google",
     FRONTEND_URL: "http://localhost:3000",
 }
+
+// export default {
+//     BACKEND_URL: "http://localhost:5000",
+//     GOOGLE : "http://localhost:5000/auth/google",
+//     FRONTEND_URL: "http://localhost:3000",
+// }
