4798b08cc1e2da2a3183e004571750914210b666
Yash Kandoi <101992822+ykandoi20330@users.noreply.github.com>
2024-01-01 23:02:35 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
-// export default {
-//     BACKEND_URL: "https://acquify-test.onrender.com",
-//     GOOGLE : "https://acquify-test.onrender.com/auth/google",
-//     FRONTEND_URL: "http://localhost:3000",
-// }
-
 export default {
-    BACKEND_URL: "http://localhost:5000",
-    GOOGLE : "http://localhost:5000/auth/google",
+    BACKEND_URL: "https://acquify-test.onrender.com",
+    GOOGLE : "https://acquify-test.onrender.com/auth/google",
     FRONTEND_URL: "http://localhost:3000",
 }
+
+// export default {
+//     BACKEND_URL: "http://localhost:5000",
+//     GOOGLE : "http://localhost:5000/auth/google",
+//     FRONTEND_URL: "http://localhost:3000",
+// }
