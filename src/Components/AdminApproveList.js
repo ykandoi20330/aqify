3e7c452ba764427ac38c40aa1844317eea539e04
Akshat Gupta <akshatgtc@gmail.com>
2024-01-07 22:50:51 +0530

    feed

--- src/Components/AdminApproveList.js
+++ src/Components/AdminApproveList.js
@@ -1,7 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import ENV from "../config.js";
 
 const AdminApproveList = () => {
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        getCards();
+    }, []);
+
+    const getCards = async () => {
+        try {
+            const response = await axios.get(
+                `${ENV.BACKEND_URL}/business/getApprovedbusiness`
+            );
+            console.log(response.data.business);
+            setCards(response.data.business);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            const response = await axios.post(
+                `${ENV.BACKEND_URL}/business/deny`, { id: id });
+            console.log(response.data.business);
+            getCards();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <>
             <div className='d-flex flex-column align-items-start'>
@@ -11,23 +42,17 @@ const AdminApproveList = () => {
                 </div>
 
                 <div className='w-100'>
-                    <div className="Approvedcard py-5" style={{width:'95%'}}>
-                        <div className='d-flex justify-content-between align-items-center py-2 px-5 rounded-pill my-2' style={{background:'#EEF0FE'}}>
-                            <ul className='my-0'>
-                                <li>Ecommece</li>
-                            </ul>
-                            <div>
-                                <Link to="#" style={{textDecoration:'none'}} className='btn btn-danger rounded-pill py-1'>Delete</Link>
+                    <div className="Approvedcard py-5" style={{ width: '95%' }}>
+                        {cards.map((card, index) => (
+                            <div key={index} className='d-flex justify-content-between align-items-center py-2 px-5 rounded-pill my-2' style={{ background: '#EEF0FE' }}>
+                                <ul className='my-0'>
+                                    <li>{card.businessName}</li> 
+                                </ul>
+                                <div>
+                                    <Link to="#" style={{ textDecoration: 'none' }} className='btn btn-danger rounded-pill py-1' onClick={() => handleDelete(card._id)}>Delete</Link>
+                                </div>
                             </div>
-                        </div>
-                        <div className='d-flex justify-content-between align-items-center py-2 px-5 rounded-pill my-2' style={{background:'#EEF0FE'}}>
-                            <ul className='my-0'>
-                                <li>Ecommece</li>
-                            </ul>
-                            <div>
-                                <Link to="#" style={{textDecoration:'none'}} className='btn btn-danger rounded-pill py-1'>Delete</Link>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </div>
@@ -35,4 +60,4 @@ const AdminApproveList = () => {
     )
 }
 
-export default AdminApproveList
+export default AdminApproveList;
--- src/Components/AdminFeedback.js
+++ src/Components/AdminFeedback.js
@@ -1,6 +1,29 @@
-import React from 'react'
+import React , {useState , useEffect} from 'react'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import ENV from "../config.js";
 
 const AdminFeedback = () => {
+
+  const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        getCards();
+    }, []);
+
+    const getCards = async () => {
+        try {
+            const response = await axios.get(
+                `${ENV.BACKEND_URL}/users/getFeedback`
+            );
+            console.log(response.data);
+            setCards(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
   return (
     <>
       <div className='d-flex flex-column align-items-start'>
