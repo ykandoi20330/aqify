badbe4622523554d8b581ff06e9ccc05d2c0ebc1
Simply Done <abcdeffect1212@gmail.com>
2024-01-07 23:56:10 +0530

    feed

--- src/Components/AdminFeedback.js
+++ src/Components/AdminFeedback.js
@@ -1,37 +1,83 @@
-import React , {useState , useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import ENV from "../config.js";
+import { jwtDecode } from "jwt-decode";
 
 const AdminFeedback = () => {
 
   const [cards, setCards] = useState([]);
 
-    useEffect(() => {
-        getCards();
-    }, []);
+  useEffect(() => {
+    getCards();
+  }, []);
 
-    const getCards = async () => {
+  const getCards = async () => {
+    try {
+      const response = await axios.get(
+        `${ENV.BACKEND_URL}/users/getFeedback`
+      );
+      console.log(response.data);
+      setCards(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const [pic, setPic] = React.useState("");
+
+
+  useEffect(() => {
+    const getUser = async () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        const decoded = jwtDecode(token);
+        const id = decoded.id;
         try {
-            const response = await axios.get(
-                `${ENV.BACKEND_URL}/users/getFeedback`
-            );
-            console.log(response.data);
-            setCards(response.data);
+          const response = await axios.get(
+            `${ENV.BACKEND_URL}/users/getUser`,
+            {
+              headers: { "x-auth-token": id },
+            }
+          );
+          setPic(response.data.user.pic);
         } catch (error) {
-            console.error(error);
+          console.error(error);
         }
+      }
     };
+    getUser();
+  }, []);
 
 
   return (
     <>
-      <div className='d-flex flex-column align-items-start'>
-        <div className='d-flex justify-content-center flex-column' style={{ height: '20vh', background: '#eef0fe' }}>
-          <h1 className='mx-3' style={{ fontWeight: '700' }}>Feedback data review</h1>
-          <span className='mx-3' style={{ color: '#636363' }}>View and edit all feed settings</span>
+      <section>
+        <div className='d-flex flex-column align-items-start'>
+          <div className='d-flex justify-content-center flex-column' style={{ height: '20vh', background: '#eef0fe' }}>
+            <h1 className='mx-3' style={{ fontWeight: '700' }}>Feedback data review</h1>
+            <span className='mx-3' style={{ color: '#636363' }}>View and edit all feed settings</span>
+          </div>
         </div>
-      </div>
+
+        <div>
+          {cards.map((item, index) => {
+            return (
+              <div key={index} className='AdminFeedbackcard'>
+                <div className='d-flex align-items-center'>
+                  <img width={50} src={pic} alt="" />
+                  <div>
+                    <h4 className='mx-2'>{item.username}</h4>
+                  </div>
+                </div>
+                <div>
+                  {item.feed}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </section>
     </>
   )
 }
