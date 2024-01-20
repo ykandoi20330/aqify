94602777eb39196d2895812f45bbe70b6a0a0344
Akshat Gupta <akshatgtc@gmail.com>
2024-01-20 17:23:30 +0530

    env updated

--- src/config.js
+++ src/config.js
@@ -1,17 +1,15 @@
-// export default {
-//     BACKEND_URL: "https://acquify-test.onrender.com",
-//     GOOGLE : "https://acquify-test.onrender.com/auth/google",
-//     FRONTEND_URL: "https://acqify.co/",
-//     AGORA_APP_ID: "dddb85ef72bb477da91d180b9f804357",
-//     WEBSOCKET_URL: "wss://acquify-test.onrender.com",
-// }
-
 export default {
-    BACKEND_URL: "http://localhost:5000",
-    GOOGLE : "http://localhost:5000/auth/google",
-    FRONTEND_URL: "http://localhost:3000",
-    AGORA_APP_ID: "dddb85ef72bb477da91d180b9f804357",
-    WEBSOCKET_URL: "ws://localhost:5000",
-}
+  BACKEND_URL: "https://aqify-backend.onrender.com",
+  GOOGLE: "https://aqify-backend.onrender.com/auth/google",
+  FRONTEND_URL: "https://acqify.co/",
+  AGORA_APP_ID: "dddb85ef72bb477da91d180b9f804357",
+  WEBSOCKET_URL: "wss://aqify-backend.onrender.com",
+};
 
-
+// export default {
+//     BACKEND_URL: "http://localhost:5000",
+//     GOOGLE : "http://localhost:5000/auth/google",
+//     FRONTEND_URL: "http://localhost:3000",
+//     AGORA_APP_ID: "dddb85ef72bb477da91d180b9f804357",
+//     WEBSOCKET_URL: "ws://localhost:5000",
+// }
